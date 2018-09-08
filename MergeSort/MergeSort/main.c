4f7ed65ec04232db41dfca208cdc1a00f3aa7f82
raghav <trk0293@gmail.com>
2018-09-07 19:29:13 -0700

    Merge sort example init

---
+++ MergeSort/MergeSort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MergeSort
+//
+//  Created by raghav on 9/7/18.
+//  Copyright © 2018 raghav. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
