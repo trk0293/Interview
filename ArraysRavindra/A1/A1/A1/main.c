34e11b280c65650635a462754466a045f17e08c5
raghav <trk0293@gmail.com>
2018-11-04 10:26:39 -0800

    Added factorial calculation API

--- ArraysRavindra/A1/A1/A1/main.c
+++ ArraysRavindra/A1/A1/A1/main.c
@@ -23,11 +23,22 @@ void isPrime(int n){
     else
         printf("%d is not a prime number \n",n);
 }
-
+long long unsigned int factorial(int n){
+    long long unsigned int factorial = 1;
+    for(int i=2;i<=n;i++)
+        factorial=factorial*i;
+    return factorial;
+}
 int main(int argc, const char * argv[]) {
     int n=0;
     printf("enter a positive number:");
     scanf("%d",&n);
+    /*prime
     isPrime(n);
+     */
+    if (n<0)
+        printf("factorial can be calculated for positive integers only. \nTry entering positive integer and run program again. \n");
+    else
+    printf("factorial of %d is %lld \n ",n,factorial(n));
     return 0;
 }
