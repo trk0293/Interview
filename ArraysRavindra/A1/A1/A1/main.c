6938ef7477f69d64d19d9c503397dbcc9dba8777
raghav <trk0293@gmail.com>
2018-11-04 10:48:46 -0800

    Half pyrmaid

--- ArraysRavindra/A1/A1/A1/main.c
+++ ArraysRavindra/A1/A1/A1/main.c
@@ -23,22 +23,38 @@ void isPrime(int n){
     else
         printf("%d is not a prime number \n",n);
 }
+
 long long unsigned int factorial(int n){
     long long unsigned int factorial = 1;
     for(int i=2;i<=n;i++)
         factorial=factorial*i;
     return factorial;
 }
+void halfPyramid(int n){
+    for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        printf("* ");
+    }
+    printf("\n");
+}
+    return;
+}
 int main(int argc, const char * argv[]) {
     int n=0;
     printf("enter a positive number:");
     scanf("%d",&n);
-    /*prime
+    /************************************************************************************************/
+    /*PRIME
     isPrime(n);
      */
+    /************************************************************************************************/
+    /* FACTORIAL
     if (n<0)
-        printf("factorial can be calculated for positive integers only. \nTry entering positive integer and run program again. \n");
+        printf("Factorial can be calculated for positive integers only. \nTry entering positive integer and run program again. \n");
     else
     printf("factorial of %d is %lld \n ",n,factorial(n));
+     */
+    /************************************************************************************************/
+    halfPyramid(n);
     return 0;
 }
