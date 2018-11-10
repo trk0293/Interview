189466bcc3faf6bdf1ead0020e75ad644db93ac2
raghav <trk0293@gmail.com>
2018-11-10 13:26:27 -0800

    [INT] issue maybe with merge sort logic/calling merge from another program

---
+++ ArraysInterview/Mergesort.c
@@ -0,0 +1,9 @@
+//
+//  Mergesort.c
+//  ArraysInterview
+//
+//  Created by raghav on 11/10/18.
+//  Copyright © 2018 raghav. All rights reserved.
+//
+
+#include "Mergesort.h"
---
+++ ArraysInterview/Mergesort.h
@@ -0,0 +1,14 @@
+//
+//  Mergesort.h
+//  ArraysInterview
+//
+//  Created by raghav on 11/10/18.
+//  Copyright © 2018 raghav. All rights reserved.
+//
+
+#ifndef Mergesort_h
+#define Mergesort_h
+
+#include <stdio.h>
+
+#endif /* Mergesort_h */
