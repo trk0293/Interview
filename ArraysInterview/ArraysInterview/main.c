21a5d0e6f982cf9387c7e2b1cffe41e1ab784a0c
raghav <trk0293@gmail.com>
2018-11-10 09:30:10 -0800

    index of array - II ; Time complexity 0(n) - Hash solution

--- ArraysInterview/ArraysInterview/main.c
+++ ArraysInterview/ArraysInterview/main.c
@@ -6,25 +6,28 @@
 //  Copyright © 2018 raghav. All rights reserved.
 //
 
+// Time complexity o(n) ; space complexity :o(n)
 #include <stdio.h>
 #include <stdlib.h>
 
-#define sum 10
+#define sum 15
+#define max 20 // max represent the maximum value in the input array
 
-void numbersSum(int* arr,int size){
-    int i,j=0;
+void numberSum(int* arr,int size)
+{
+    int i,temp=0;
+    int hash[max]={0};
     for(i=0;i<size;i++){
-        for(j=i+1;j<size;j++){
-            if (*(arr+i)+*(arr+j)==sum)
-                printf("Numbers that add up to sum are at index %d and %d \n",i+1,j+1); // assuming 0 == 1
-        }
+        temp = sum-*(arr+i);
+        if(temp == hash[temp] && (temp!=0)) printf("Numbers that add upto %d are at %d,%d\n",sum,i+1,temp);
+        hash[*(arr+i)] = *(arr+i);
     }
 }
 
 int main(int argc, const char * argv[]) {
     int arr[] = {1,2,3,4,5,6,7,10};
     int size = sizeof(arr)/ sizeof(arr[0]);
-    numbersSum(arr, size);
-    //printf("Key is at index %d in array\n",linearSearch(arr, sizeof(arr)));
+    numberSum(arr, size);
+    
     return 0;
 }
