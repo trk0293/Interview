2a51e575a95a0719833b463caca8a2000f9137b8
raghav <trk0293@gmail.com>
2018-11-10 08:36:53 -0800

    Index of array that sum to n

--- ArraysInterview/ArraysInterview/main.c
+++ ArraysInterview/ArraysInterview/main.c
@@ -9,24 +9,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define Key 10
+#define sum 10
 
-void binarySearch(int* arr,int l,int r){
-    if(l==r | r>l){
-    if(*(arr+(l+r)/2) == Key)
-        printf("key found at index:%d \n",(l+r)/2); 
-    else if(*(arr+(l+r)/2)>Key)
-        binarySearch(arr,l,(l+r/2));
-    else
-        binarySearch(arr,(l+r)/2,r);
+void numbersSum(int* arr,int size){
+    int i,j=0;
+    for(i=0;i<size;i++){
+        for(j=i+1;j<size;j++){
+            if (*(arr+i)+*(arr+j)==sum)
+                printf("Numbers that add up to sum are at index %d and %d \n",i+1,j+1); // assuming 0 == 1
+        }
     }
-    
 }
 
 int main(int argc, const char * argv[]) {
     int arr[] = {1,2,3,4,5,6,7,10};
-    int n = sizeof(arr)/ sizeof(arr[0]);
-    binarySearch(arr, 0,n);
+    int size = sizeof(arr)/ sizeof(arr[0]);
+    numbersSum(arr, size);
     //printf("Key is at index %d in array\n",linearSearch(arr, sizeof(arr)));
     return 0;
 }
