f4c952ce47066de9bc74f096b8393335c96cf1ed
raghava <trk0293@gmail.com>
2018-11-12 21:43:54 -0800

    Stage -1 Moore voting algorithm completed

--- ArraysInterview/ArraysInterview/main.c
+++ ArraysInterview/ArraysInterview/main.c
@@ -4,14 +4,39 @@
 
 
 #include<stdio.h>
+#include<stdlib.h>
 
 int findMajority(int* arr, int size){
+	int* dupArr=(int*)malloc(sizeof(arr));
+	int i,count=0;
+	int index = *(arr);
+	/* Moore voting algorithm sub -1 end*/
+	for(i=0;i<size;i++){
+		if(*(arr+i)==index)
+			{
+				count++;
+				dupArr[index]=count;
+			}
+		else if(*(arr+i)!=index && dupArr[index]>0){
+				count--;
+				dupArr[index]=count;
+			}
+		else{
+				index = *(arr+i);
+				dupArr[index]=1;
+		}		
+	}
+	printf("%d \n",index);
+	
+	/* check if Moore's decision is true sub-2 end*/
+
 
 }
 
 
+
 int main(int argc, const char * argv[]){
-	int arr[]={1,1,1,1,2,3,4};
+	int arr[]={2,2,2,2,2,3,4};
 	int size=sizeof(arr)/sizeof(arr[0]);
 	findMajority(arr,size);
 	return 0;
