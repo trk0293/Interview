b44c3e64b32ad63df25817cdae65132c57016608
raghava <trk0293@gmail.com>
2018-11-12 22:02:31 -0800

    o(n) solution to find majority

--- ArraysInterview/ArraysInterview/main.c
+++ ArraysInterview/ArraysInterview/main.c
@@ -1,6 +1,5 @@
 // Moore voting algorithm to find the majority element in an array. 
-
-
+// T(n)=o(n);s(n)=o(n)
 
 
 #include<stdio.h>
@@ -26,17 +25,20 @@ int findMajority(int* arr, int size){
 				dupArr[index]=1;
 		}		
 	}
-	printf("%d \n",index);
-	
+	// printf("%d \n",index); // Un-coment to test Moore voting
+	count=0;
 	/* check if Moore's decision is true sub-2 end*/
-
-
+	for (i=0;i<size;i++){
+		if(*(arr+i)==index) count++;
+	}
+	if(count > size/2) printf("%d is majority in array \n",index);
+	else printf("There is no majority\n");
 }
 
 
 
 int main(int argc, const char * argv[]){
-	int arr[]={2,2,2,2,2,3,4};
+	int arr[]={2,2,2,3,3,33,3,3,3,3,3,3,1,3,4};
 	int size=sizeof(arr)/sizeof(arr[0]);
 	findMajority(arr,size);
 	return 0;
