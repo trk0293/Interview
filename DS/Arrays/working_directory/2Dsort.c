51760a9b50ae4e395aaa1098fce1c7e281630bb1
raghava <trk0293@gmail.com>
2019-09-14 15:05:07 -0700

    [LC] 2D sort

---
+++ DS/Arrays/working_directory/2Dsort.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, const char* argv[]){
+int arr[4][5] = {{5,12,17,21,23},
+	{1,2,4,6,8},
+	{12,14,18,19,27},
+	{3,7,9,15,25}};
+
+return 0;
+}
